03542005a722c6fd7a3db5a69c91077e77cd7b0d
Julio Fraire <julio.fraire@gmail.com>
2018-03-12 19:46:28 +0100

    Added POD documentation.

--- lib/AnyEvent/Modbus/Server.pm
+++ lib/AnyEvent/Modbus/Server.pm
@@ -24,7 +24,7 @@ sub process_request {
     # we read the body of the request.
     $handle->unshift_read( chunk => 8, sub {
         my ($handle, $data) = @_;
-        
+
         # Read header
         my ($id, $proto, $length, $unit, $code) = unpack 'nnnCC', $data;
         my $request;
@@ -49,7 +49,7 @@ sub process_request {
                 unit    => $unit,
                 message => $response
             );
-            
+
             # Send the response to the client
             $handle->push_write($adu->binary_message);
         };
@@ -75,7 +75,7 @@ sub process_request {
                         exception_code => 3
                     );
                 }
-                else {               
+                else {
                     $request = Device::Modbus::Request->new(
                         code       => $code,
                         address    => $address,
@@ -165,10 +165,10 @@ sub process_request {
                     exception_code => 1,
                 );
             }
-            
+
             # Process the request. $_[0] is the handle; $process_request
             # is the code ref built above and it includes the $request
-            # object already.            
+            # object already.
             $process_request->($_[0]);
         });
     });
@@ -185,17 +185,79 @@ sub log {
 
 =head1 NAME
 
-AnyEvent::Modbus::Server - Asynchronous MODBUS server implementation
+AnyEvent::Modbus::Server - An asynchronous MODBUS server implementation
 
 =head1 SYNOPSIS
 
+ use Device::Modbus::AnyEvent::TCP::Server;
+ use Application::Unit;
+ use strict;
+ use warnings;
+
+ my $server = Device::Modbus::AnyEvent::TCP::Server->new(
+    host => '127.0.0.1',
+    port => 8765,
+ );
+
+ my $unit = Application::Unit->new(id => 1);
+ $server->add_server_unit($unit);
+
+ my $cv = AnyEvent->condvar;
+
+ my $guard = $server->start;
+
+ my $exit = AnyEvent->signal(
+    signal => 'INT',
+    cb     => sub {
+        undef $guard;
+        $cv->send;
+    }
+ );
+
+ $cv->recv;
+
 =head1 DESCRIPTION
 
-=head1 METHODS
+This distribution proposes an asynchronous MODBUS TCP server built on top of L<Device::Modbus::Server> and L<AnyEvent>. The unit-based interface has been kept.
+
+For a proper explanation of the server, please refer to L<Device::Modbus::Server>. This document will discuss only the constructor of the asynchronous Modbus TCP. As of this writing, the asynchronous RTU version has not been developed.
+
+=head1 MODBUS TCP SERVER
+
+=head2 Constructor
+
+The server is built on the C<tcp_server> proposed by L<AnyEvent::Socket>, and it only supports two attributes: C<host> and C<port>:
+
+ my $server = Device::Modbus::AnyEvent::TCP::Server->new(
+   host => '10.10.0.53', # Default is 127.0.0.1
+   port => 8765,         # Default is 502
+ );
+
+Just like attributes, the server has only a few methods. Please see L<Device::Modbus::Server> for a discussion on how they all fit together.
+
+=head2 Adding a Server Unit
+
+As you can see in the synopsis, after instantiating the server you must define at least one server unit. A server unit is a class that specifies the functionality of the server. It is a mapping between client requests and server-side work.
+
+ my $unit = Application::Unit->new( id => 33 );
+ $server->add_server_unit( $unit );
+
+All there is left to do is to launch the server.
+
+=head2 Starting the server
+
+ This is as simple as calling the start method. It will return a I<guard> object whose life is tied to that of the server. The server will be stopped if this object is destroyed.
+
+ my $guard = $server->start;
+
+And that is it, your MODBUS server should be listening in the required port.
 
 =head1 SEE ALSO
 
+L<Device::Modbus>, L<Device::Modbus::Server>, L<AnyEvent>, L<AnyEvent::Socket>.
+
 =head1 LICENSE
 
-=cut
+Please refer to the LICENSE file you received with this distribution.
 
+=cut
